58f12ebaccf737420160523b8709ea44554ac432
Piyush2627 <piyushdahiwadkar89@gmail.com>
2022-03-13 22:17:40 +0530

    update

--- gym/index.js
+++ gym/index.js
@@ -1,1 +1,12 @@
-  
+let btnn=document.getElementById("btnn");
+function pass(){
+  let password = document.getElementById('passwordid').value;
+    console.log(password);
+  if (password== 'shiv')
+  {
+      console.log("pass")
+      location.href = "login.html"
+    } else {
+    console.log("fail")
+  }
+}
