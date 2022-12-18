ac85249a9dd4cde048379135d21125aab1e3ee3a
Piyush Dahiwadkar <97776547+Piyush2627@users.noreply.github.com>
2022-12-18 21:44:03 +0530

    summary

--- MyProfil/index.js
+++ MyProfil/index.js
@@ -1,8 +1,33 @@
-const btn=document.querySelectorAll('.btn')
+const btn = document.querySelectorAll(".btn");
 
-function main(){
-    const menu=document.getElementById('menu');
-    menu.classList.toggle('hidden')
-    console.log("main")
-
+function main() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("hidden");
+  console.log("main");
 }
+const dataDoughnut = {
+  labels: ["JavaScript", "Python", "Ruby"],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [300, 50, 100],
+      backgroundColor: [
+        "#112D4E",
+        "#3F72AF",
+        "#DBE2EF",
+      ],
+      hoverOffset: 4,
+    },
+  ],
+};
+
+const configDoughnut = {
+  type: "doughnut",
+  data: dataDoughnut,
+  options: {},
+};
+
+var chartBar = new Chart(
+  document.getElementById("chartDoughnut"),
+  configDoughnut
+);
